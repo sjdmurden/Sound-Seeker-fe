bfa3a0b88e1a5b6430ff8a1df77f3376b0dcba3f
sjdmurden <sjd.murden@btinternet.com>
2024-03-25 13:58:23 +0000

    festival search functional

--- Frontend/fe-soundseeker/Screens/SearchScreen.js
+++ Frontend/fe-soundseeker/Screens/SearchScreen.js
@@ -1,76 +1,52 @@
-import * as React from 'react';
-import { Searchbar, TextInput } from 'react-native-paper';
-import { useState } from 'react';
-import axios from 'axios';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Text } from 'react-native';
-import { useSearchBox } from 'react-instantsearch-hooks';
+import * as React from "react";
+import { Searchbar } from "react-native-paper";
+import { useState } from "react";
+import axios from "axios";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, TextInput } from "react-native";
 
-const SearchScreen = (props) => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [festivalResult, setFestivalResult] = useState('')
-  console.log('our search: ',searchQuery);
+const SearchScreen = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [festivalResult, setFestivalResult] = useState("");
+  console.log("our search: ", searchQuery);
 
-  const { query, refine } = useSearchBox(props);
-  const [searchValue, setSearchValue] = useState(query);
-  const inputRef = useRef(null);
-
- const updateQuery = (newQuery) => {
-    setSearchValue(newQuery);
-    refine(newQuery);
+  function searchAllFestivals(festival_name) {
+    return axios
+      .get(
+        `https://www.skiddle.com/api/v1/events?api_key=638e2af9d6545b37c5bf2afbed3261cc&eventcode=FEST&keyword=${festival_name}&description=1`
+      )
+      .then((response) => {
+        if (response.data.results.length > 0) {
+          setFestivalResult(response.data.results);
+        } else {
+          setFestivalResult("No festival found");
+        }
+        setSearchQuery("");
+      });
   }
-
-  if (query !== searchValue && !inputRef.current?.isFocused()) {
-    setSearchValue(query);
+  function handleSearch() {
+   searchAllFestivals(searchQuery)
   }
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      
       <TextInput
-        style={styles.input}
-        ref={inputRef}
-        value={searchValue}
-        onChangeText={updateQuery}
-        clearButtonMode="while-editing"
-        placeholder='Search'
+        placeholder="Search..."
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+        onSubmitEditing={handleSearch}
       />
-    </View>
+      {Object.keys(festivalResult).length > 0 && festivalResult.map((festival) => {
+
+          return (
+            <SafeAreaView key={festival.id}>
+              <Text>{festival.eventname}</Text>
+            </SafeAreaView>
+          );
+        })}
+    </SafeAreaView>
   );
-}
+};
 
-  function handleSearch(event){
-   event.preventDefault()
-     function searchAllFestivals(festival_name) {
-      return axios.get(`https://www.skiddle.com/api/v1/events?api_key=638e2af9d6545b37c5bf2afbed3261cc&eventcode=FEST&keyword=${festival_name}&description=1`)
-      .then((response) => {
-         console.log(response.data.results[0], '<-- search result')
-         if(response.data.results.length > 0){
-            setFestivalResult(response.data.results)
-         } else {
-            setFestivalResult('No festival found')
-         }
-         setSearchQuery('')
-      })
-     }
-
-  }
-
-  
-   export default SearchScreen;
-   
-   // <SafeAreaView>
-   //    <Searchbar
-   //      placeholder="Search"
-   //      onChangeText={setSearchQuery}
-   //      value={searchQuery}
-   //      onSubmitEditing = {() => searchAllFestivals(searchQuery)}
-   //    />
-   //    {festivalResult.map((festival) => {
-   //       return (
-   //          <SafeAreaView>
-   //             <Text key={festival.id}>{festival.eventname}</Text>
-   //          </SafeAreaView>
-   //       )
-   //    })}
-
-   // </SafeAreaView>
+export default SearchScreen;
