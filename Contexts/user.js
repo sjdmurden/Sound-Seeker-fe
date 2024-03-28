b4113d87c2fe41e41da3595ab0251b0748b1b098
Rayhan Elbeera <raybeera2014@gmail.com>
2024-03-28 14:40:41 +0000

    logging page styling

--- Contexts/user.js
+++
@@ -1,8 +0,0 @@
-import {createContext, useState} from 'react'
-export const UserContext = createContext()
-export const UserProvider = ({children}) => {
-    const [loggedInUser, setLoggedInUser] = useState(null)
-    return <UserContext.Provider value={{loggedInUser, setLoggedInUser}}>
-        {children}
-    </UserContext.Provider>
-}
