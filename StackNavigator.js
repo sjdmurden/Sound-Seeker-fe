e4bc722143036129dce376a5230c87d03c27f243
M1nhnho <tien.nguho@hotmail.com>
2024-04-04 17:58:25 +0100

    Added zoom out transition.

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,5 +1,4 @@
-
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "./screens/LoginScreen.js";
 import SearchScreen from "./screens/SearchScreen.js";
@@ -7,8 +6,61 @@ import FestivalPage from "./screens/FestivalPage.js";
 import * as SecureStore from "expo-secure-store";
 import { useEffect, useContext } from "react";
 import { UserContext } from "./contexts/user.js";
+import { Animated } from "react-native";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
+
+const zoomOut = ({ current, next, inverted }) => {
+  const progress = Animated.add(
+    current.progress.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 1],
+      extrapolate: 'clamp',
+    }),
+    next
+      ? next.progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 1],
+          extrapolate: 'clamp',
+        })
+      : 0
+  );
+
+  return {
+    cardStyle: {
+      transform: [
+        {
+          scaleX: Animated.multiply(
+            progress.interpolate({
+              inputRange: [0, 1, 2],
+              outputRange: [0.75, 1, 1],
+              extrapolate: 'clamp',
+            }),
+            inverted
+          )
+        },
+        {
+          scaleY: Animated.multiply(
+            progress.interpolate({
+              inputRange: [0, 1, 2],
+              outputRange: [0.25, 1, 1],
+              extrapolate: 'clamp',
+            }),
+            inverted
+          )
+        }
+      ],
+      opacity: Animated.multiply(
+        progress.interpolate({
+          inputRange: [0, 1, 2],
+          outputRange: [0, 1, 2],
+          extrapolate: 'clamp',
+        }),
+        inverted
+      )
+    },
+  };
+};
 
 function Navigation() {
   const { loggedInUser, setLoggedInUser } = useContext(UserContext);
@@ -38,7 +90,10 @@ function Navigation() {
             <Stack.Screen
               name="FestivalPage"
               component={FestivalPage}
-              options={{ headerShown: true}}
+              options={{
+                headerShown: true,
+                cardStyleInterpolator: zoomOut,
+              }}
             />
           </>
         ) : (
